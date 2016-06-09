6d87f757f55d60a4964f4b72d7e6e90dcc5d6c74
Jonathan Doig <jonathan.doig@gmail.com>
2016-06-10 08:20:08 +1000

    47 Convert input to popup 2/2

--- js/findelec.js
+++ js/findelec.js
@@ -2,14 +2,20 @@ var locs,
   pcLocs,
   elecs;
 
- var locFilename = "localtest.json";
-//var locFilename = "localities.json";
+//var locFilename = "localtest.json";
+var locFilename = "localities.json";
 var elecFilename = "electorates.json";
+
+var map, label, symbol;
 
 //var ozExtent = {"xmin": 112.921112, "ymin": -54.640301,
 //                "xmax": 159.278717, "ymax": -9.22882};
-var ozExtent = {"xmin": 112.296895, "ymin": -44.006562,
-                "xmax": 154.915372, "ymax": -9.599248};
+var ozExtent = {
+  "xmin": 112.296895,
+  "ymin": -44.006562,
+  "xmax": 154.915372,
+  "ymax": -9.599248
+};
 
 loadJson(locFilename, function (json) {
   locs = json;
@@ -29,7 +35,120 @@ function loadJson(url, func) {
   xhttp.send();
 }
 
-function findPc(pc) {
+var findPc, drawMap;
+
+function initMap() {
+  require([
+        "esri/map",
+        "esri/dijit/HomeButton",
+        "esri/layers/FeatureLayer",
+        "esri/geometry/Extent",
+        "esri/symbols/TextSymbol",
+        "esri/symbols/Font",
+        "esri/layers/LabelClass",
+        "esri/Color",
+        "esri/symbols/SimpleMarkerSymbol",
+        "esri/renderers/SimpleRenderer",
+        "esri/symbols/SimpleFillSymbol",
+        "esri/symbols/SimpleLineSymbol",
+        "dojo/domReady!"
+    ], function (Map, HomeButton, FeatureLayer, Extent, TextSymbol, Font, LabelClass, Color, SimpleMarkerSymbol, SimpleRenderer, SimpleFillSymbol, SimpleLineSymbol) {
+
+    var drawColor = new Color("#008060");
+
+    var urlPrefix = "https://services5.arcgis.com/BZoOjszBbEr9f2ol/arcgis/rest/services/Australian_Federal_Electorates_2016_",
+      urlSuffix = "/FeatureServer/0";
+
+    var states = {
+      ACT: "Australian Capital Territory",
+      NSW: "New South Wales",
+      NT: "Northern Territory",
+      QLD: "Queensland",
+      SA: "South Australia",
+      TAS: "Tasmania",
+      VIC: "Victoria",
+      WA: "Western Australia"
+    };
+
+    // Label field capitalisation is inconsistent and it matters
+    var labelFields = {
+      ACT: "Elect_div",
+      NSW: "Elect_div",
+      NT: "Elect_div",
+      QLD: "ELECT_DIV",
+      SA: "ELECT_DIV",
+      TAS: "Elect_div",
+      VIC: "ELECT_DIV",
+      WA: "Elect_div"
+    };
+
+    map = new Map("viewDiv", {
+      basemap: "streets",
+      //      extent: new Extent(west, south, east, north),
+      showLabels: true,
+      extent: new Extent(ozExtent),
+      fitExtent: true,
+      minScale: 40000000, // User cannot zoom out beyond 1:40m (Australia)
+      maxScale: 9000 // User cannot zoom in beyond 1:9k (street)
+    });
+
+    //create symbol to draw the electorates layer
+    symbol = new SimpleFillSymbol(
+      SimpleFillSymbol.STYLE_SOLID,
+      new SimpleLineSymbol(
+        SimpleLineSymbol.STYLE_SOLID,
+        new Color([0, 128, 96, 1]), 3
+      ),
+      new Color([0, 0, 0, 0])
+    );
+
+    // create a text symbol to define the style of labels
+    label = new TextSymbol().setColor(drawColor);
+    //    label.font.setSize("14pt");
+    label.font.setSize("18pt");
+    label.font.setFamily("arial");
+    label.font.setWeight(Font.WEIGHT_BOLD);
+    label.setHaloColor(new Color("white"));
+    label.setHaloSize(5);
+
+    var home = new HomeButton({
+      map: map
+    }, "HomeButton");
+    home.startup();
+
+    drawMap = function (state, extent) {
+      map.setExtent(new Extent(extent));
+
+      var url = urlPrefix + states[state].replace(/ /g, "_") + urlSuffix;
+      var lyr = new FeatureLayer(url, {
+        id: "lyr",
+        outFields: [labelFields[state]]
+      });
+
+      lyr.setRenderer(new SimpleRenderer(symbol));
+
+      // create a text symbol to define the style of labels
+      var labelJson = {
+        "labelExpressionInfo": {
+          "value": "{" + labelFields[state] + "}"
+        }
+      };
+      var labelClass = new LabelClass(labelJson);
+      labelClass.symbol = label;
+      lyr.setLabelingInfo([labelClass]);
+
+      lyr.on("click", function (evt) {
+        var attrs = evt.graphic.attributes;
+        var elec = attrs[Object.keys(attrs)[0]]; // First and only attribute
+        showElec(elec);
+      });
+
+      map.addLayer(lyr);
+    }
+  });
+}
+
+findPc = function (pc) {
   var pc = pc.replace('\n', '').trim();
   var elem = document.getElementById("locOptions");
   var options = "<option hidden></option>\n";
@@ -65,7 +184,8 @@ function findPc(pc) {
       if ("e" in pcLocs[0]) { // If there is an electorate, show it
         showElec(pcLocs[0].e, pcLocs[0]);
       } else { // Otherwise map the locality
-        drawMap(map, pcLocs[0].s, pcLocs[0].x);
+        document.getElementById("inputPanel").style.display = "none";
+        drawMap(pcLocs[0].s, pcLocs[0].x);
         locText = titleCase(pcLocs[0].l) + " " + pcLocs[0].p;
         document.getElementById("mapHeader").innerHTML =
           "Where in " + locText.trim() + "? Click map.";
@@ -90,8 +210,8 @@ function findLoc(l) {
       if ("e" in pcLocs[i]) {
         showElec(pcLocs[i].e, pcLocs[i]);
       } else {
-        //                        h.innerHTML = "Loading map...";
-        drawMap(map, pcLocs[i].s, pcLocs[i].x);
+        document.getElementById("inputPanel").style.display = "none";
+        drawMap(pcLocs[i].s, pcLocs[i].x);
         document.getElementById("mapHeader").innerHTML =
           "Where in " + titleCase(l) + "? Click map.";
       }
@@ -101,9 +221,9 @@ function findLoc(l) {
 }
 
 function showElec(elec, loc) {
-  
+
   document.getElementById("inputPanel").style.display = "none";
-  
+
   var elecDiv = document.getElementById("elecPanel");
 
   if (loc) {
