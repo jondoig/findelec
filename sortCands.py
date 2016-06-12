970592036262140e2d214678c076823e19c30ffe
Jonathan Doig <jonathan.doig@gmail.com>
2016-06-12 20:59:45 +1000

    60: Mark up cands - sort
    Sort by major parties first then party, then name

--- sortCands.py
+++ sortCands.py
@@ -14,11 +14,29 @@ with open(infilename + '.json') as elecfile:
 
 elecs_out = {}
 
+# Sort majors first, then by party, then by name
+def compareCand(a, b):
+  majors = ['Greens', 'Labor', 'Lib', 'Nat']
+  if a['p'] in majors and not b['p'] in majors:
+    return -1
+  elif b['p'] in majors and not a['p'] in majors:
+    return 1
+  elif a['p'] == b['p']:
+    if a['n'] < b['n']:
+      return -1
+    else:
+      return 1
+  else:
+    if a['p'] < b['p']:
+      return -1
+    else:
+      return 1
+
 # For each electorate
 for elec, cands in elecs_in.iteritems():
-#  cands.sort(key=lambda x: x.p)
-  elecs_out[elec] = sorted(cands)
-  elecs_out[elec] = sorted(cands, key=lambda x: x['p'])
+#  elecs_out[elec] = sorted(cands, key=lambda x: x['p'])
+  elecs_out[elec] = sorted(cands, cmp=compareCand)
+#  elecs_out[elec] = cands.sort(compareCand)
 
 #Write sorted electorates to json file
 with open(outfilename + '.json', 'w') as outfile:
